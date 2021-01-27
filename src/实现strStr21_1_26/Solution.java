43ef5d0d445eaa5537ffa93795b3ae587e0bf2e7
zhang <11358995237@qq.com>
2021-01-27 22:14:44 +0800

    实现strStr() 重新提交

--- src/实现strStr21_1_26/Solution.java
+++ src/实现strStr21_1_26/Solution.java
@@ -10,17 +10,13 @@ public class Solution {
         if (needle == null || "".equals(needle)) {
             return 0;
         }
-        if(needle.length() > haystack.length()){
-            return -1;
-        }
 
-        for (int i = 0; i < haystack.length(); i++) {
-            int index = 0;
-            for (int j = i; j < i + needle.length() && j < haystack.length(); j++) {
-                if (haystack.charAt(j) != needle.charAt(j - i)) {
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            for (int j = 0; j < haystack.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
                     break;
                 }
-                if (++index == needle.length()) {
+                if (j == needle.length() - 1) {
                     return i;
                 }
             }
