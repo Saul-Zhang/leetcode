7839a75e8e126c5b38d7bc741ffe3a04687d4e79
ZhangSong <1358995237@qq.com>
2022-07-10 14:05:57 +0800

    不同路径

---
+++ src/不同路径62/Solution.java
@@ -0,0 +1,28 @@
+package 不同路径62;
+
+/**
+ * @author Zhang Song
+ * @date 2022/7/10 11:32
+ */
+public class Solution {
+  public int uniquePaths(int m, int n) {
+    if (m == 1 || n == 1) {
+      return 1;
+    }
+    int[][] dp = new int[m][n];
+    for (int i = 0; i < m; i++) {
+      dp[i][0] = 1;
+    }
+    for (int i = 0; i < n; i++) {
+      dp[0][i] = 1;
+    }
+
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        dp[i][j] = dp[i-1][j] + dp[i][j-1];
+      }
+    }
+    return dp[m-1][n-1];
+  }
+
+}
